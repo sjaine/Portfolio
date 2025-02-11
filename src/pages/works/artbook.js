197cca0b0427d4651f3978ae8f69805d744b2389
Jaine <jaineee00@gmail.com>
2025-02-11 13:32:33 -0500

    ArtBook Project details

--- src/pages/works/artbook.js
+++ src/pages/works/artbook.js
@@ -1,6 +1,14 @@
 import React, { useRef, useState } from 'react';
 import * as motion from "motion/react-client"
 import ExternalNav from '../../components/ExternalNav';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Pagination, Autoplay } from 'swiper/modules';
 
 import artbook from '../../assets/img/artbook.png';
 
@@ -83,7 +91,7 @@ Hand-coded with ❤️ using React & Motion</div>
                 <div className="title color_blue" >ArtBook.</div>
                 <div className="project_content">
                     <div className="main color_text project_subject">&lt;overview&gt;</div> 
-                    <div className="body project_desc line_height">I developed a website that recommends artworks tailored to users' preferences by integrating the MET API. I utilized React to manage and integrate the entire HTML/CSS structure seamlessly. <br />
+                    <div className="body project_desc line_height">I developed a website that recommends artworks tailored to users' preferences by integrating the <b>MET API.</b> I utilized <b>React</b> to manage and integrate the entire HTML/CSS structure seamlessly. <br /><br />
                     Collaborating closely with my colleague, a UI/UX designer, provided me with valuable experience in teamwork and cross-functional collaboration.</div>
                 </div>
             </motion.div>
@@ -131,7 +139,7 @@ Hand-coded with ❤️ using React & Motion</div>
             {/* deliver */}
             <motion.div
                 className="project_body">
-                <div className="body_title main color_text">📁 ../deliver/final_solution</div>
+                <div className="body_title main color_text">📁 ../deliver/output</div>
             </motion.div>
 
             <div className="project_deliver" 
@@ -187,6 +195,148 @@ Hand-coded with ❤️ using React & Motion</div>
                     </div>
                 </div>
             </div>
+
+            {/* features */}
+            <div className="project_body">
+                    <div className="deliver_container">
+                        <motion.div
+                            initial={{ y: 50, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{ type: "tween", stiffness: 150 }} 
+                            className="deliver_box box_pc line_height"
+                        >
+                            <div>
+                                <div className="deliver_desc main color_text">[Sign In & Up]</div>
+                                <ul className="body">
+                                    <li><b>Validation:</b> Checks if the chosen nickname already exists in the MongoDB database.</li>
+                                    <li><b>Customization:</b> Users can select an emoji and nickname for a fun, personalized experience.</li>
+                                    <li><b>Authentication:</b> Implements sign-in and sign-up functionality.</li>
+                                </ul>
+                            </div>
+
+                            <Swiper 
+                            pagination={true}
+                            modules={[Pagination, Autoplay]} 
+                            loop={true}
+                            autoplay={{ delay: 3000, disableOnInteraction: false }}
+                            className="deliver_img_pc mySwiper">
+                                <SwiperSlide>
+                                    <img src={final1} alt="final 1" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final2} alt="final 2" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final3} alt="final 3" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final4} alt="final 4" className="img_pc" />
+                                </SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+
+                        <motion.div
+                            initial={{ y: 50, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{ type: "tween", stiffness: 150 }} 
+                            className="deliver_box box_pc line_height"
+                        >
+                            <div>
+                                <div className="deliver_desc main color_text">[Artwork Lists]</div>
+                                <ul className="body">
+                                    <li><b>Data Fetching:</b> Retrieves artwork information from the MET API.</li>
+                                    <li><b>Filtering:</b> Displays the second artwork’s thumbnail for each category.</li>
+                                    <li><b>Recommendations:</b> Shows the top 5 matching artworks in a table.</li>
+                                    <li><b>User Interaction:</b> Clicking on an artwork provides more details and allows users to add it to their favorites, saving the selection in the MongoDB database.</li>
+                                </ul>
+                            </div>
+
+                            <Swiper 
+                            pagination={true}
+                            modules={[Pagination, Autoplay]} 
+                            loop={true}
+                            autoplay={{ delay: 3000, disableOnInteraction: false }}
+                            className="deliver_img_pc mySwiper">
+                                <SwiperSlide>
+                                    <img src={final5} alt="final 5" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final6} alt="final 6" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final7} alt="final 7" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final8} alt="final 8" className="img_pc" />
+                                </SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+                        <motion.div
+                            initial={{ y: 50, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{ type: "tween", stiffness: 150 }} 
+                            className="deliver_box box_pc line_height"
+                        >
+                            <div>
+                                <div className="deliver_desc main color_text">[Saved & Recommendation]</div>
+                                <ul className="body">
+                                    <li><b>User Favorites:</b> Loads saved artworks from the MongoDB database, optimizing performance by storing only essential details.</li>
+                                    <li><b>Similar Users:</b> Displays favorite artworks of users who have chosen the same emoji.</li>
+                                    <li><b>Popular Picks:</b> Highlights the most popular artworks among users with the same emoji.</li>
+                                </ul>
+                            </div>
+
+                            <Swiper 
+                            pagination={true}
+                            modules={[Pagination, Autoplay]} 
+                            loop={true}
+                            autoplay={{ delay: 3000, disableOnInteraction: false }}
+                            className="deliver_img_pc mySwiper">
+                                <SwiperSlide>
+                                    <img src={final9} alt="final 9" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final10} alt="final 10" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final11} alt="final 11" className="img_pc" />
+                                </SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+                        <motion.div
+                            initial={{ y: 50, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{ type: "tween", stiffness: 150 }} 
+                            className="deliver_box box_pc line_height"
+                        >
+                            <div>
+                                <div className="deliver_desc main color_text">[User Account]</div>
+                                <ul className="body">
+                                    <li><b>Analytics:</b> Provides insights into the user’s top department preferences.</li>
+                                    <li><b>AI Integration (Planned):</b> Implements an AI-powered feature to offer information and the fastest route to explore favorite artworks.</li>
+                                    <li><b>Account Management:</b> Allows users to delete their accounts.</li>
+                                </ul>
+                            </div>
+
+                            <Swiper 
+                            pagination={true}
+                            modules={[Pagination, Autoplay]} 
+                            loop={true}
+                            autoplay={{ delay: 3000, disableOnInteraction: false }}
+                            className="deliver_img_pc mySwiper">
+                                <SwiperSlide>
+                                    <img src={final12} alt="final 12" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final13} alt="final 13" className="img_pc" />
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <img src={final14} alt="final 14" className="img_pc" />
+                                </SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+                    </div>
+                </div>
 
             {/* reflection */}
             <motion.div
